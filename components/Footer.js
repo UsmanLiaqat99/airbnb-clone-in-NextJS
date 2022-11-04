c98014280c0e09497c3dcc2525b36f969eb38e53
Usman Liaqat <chusmanliaqat1020@gmail.com>
2022-11-04 17:03:48 +0500

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,19 @@
+function Footer() {
+  return (
+    <div>
+      <div>
+        <h5>About</h5>
+      </div>
+
+      <div></div>
+
+      <div></div>
+
+      <div></div>
+
+      <div></div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ next.config.js
@@ -0,0 +1,9 @@
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['links.papareact.com']
+  }
+}
+
+module.exports = nextConfig
